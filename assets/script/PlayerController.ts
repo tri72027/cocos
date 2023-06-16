10a99008023f08f6d574a4639bf900e9066a61e0
tri72027 <tri72027@gmail.com>
2023-06-16 23:12:24 +0700

    fix moving

--- assets/script/PlayerController.ts
+++ assets/script/PlayerController.ts
@@ -1,8 +1,16 @@
-import { _decorator, Component, EventMouse, Node, systemEvent, SystemEventType, Vec3 } from 'cc';
+import { _decorator, Animation, Component, EventMouse, Node, systemEvent, SystemEventType, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
 export class PlayerController extends Component {
+    @property(
+        {
+            type: Animation
+        }
+    )
+
+    pika: Animation =null;
+
     private _startJump = false;         // bắt đầu có nhảy hay không
     private _jumpStep = 0;              // số bước nhảy( độ dài bước nhảy)
     private _curJumpTime = 0;           // thời gian nhảy hiện tại
@@ -11,6 +19,7 @@ export class PlayerController extends Component {
     private _curPos = new Vec3();       // vị trí hiện tại
     private _targetPos = new Vec3();    // vị trí của nhân vật
     private _deltaPos = new Vec3()      // sự kahcs biệt về chuyển động của nhân vật
+    private _isMoving = false;
     start() {
         systemEvent.on(SystemEventType.MOUSE_UP, this.onMouseUp, this)
     }
@@ -25,29 +34,44 @@ export class PlayerController extends Component {
     }
 
     jumpByStep(step: number) {
+        if(this._isMoving)
+        {
+            return
+        }
         this._startJump = true;
         this._jumpStep = step;
         this._curJumpSpeed = this._jumpStep / this._jumpTime;
         this._curJumpTime = 0;
         this.node.getPosition(this._curPos); // lấy vị trí hiện tại của nhân vật
         Vec3.add(this._targetPos, this._curPos, new Vec3(step, 0, 0));                          // lấy vị trí của mục tiêu muốn chuyển đến
+        console.log('pi: '+ this.pika)
+        if(this.pika)
+        {
+            this.pika.play('Jump');
+            console.log('pi: '+ this.pika);
+        }
     }
     update(deltaTime: number) {
-        if(this.start)
+        if(this._startJump)
         {
             this._curJumpTime += deltaTime;
             if(this._curJumpTime > this._jumpTime)
             {
                 this.node.setPosition(this._targetPos);
                 this._startJump = false
+                this.onOnceJumpEnd()
+            }
+            else{
+                this.node.getPosition(this._curPos); 
+                this._deltaPos.x = this._curJumpSpeed * deltaTime;
+                Vec3.add(this._curPos, this._curPos, this._deltaPos);   
+                this.node.setPosition(this._curPos);
             }
         }
-        else{
-            this.node.getPosition(this._curPos); 
-            this._deltaPos.x = this._curJumpSpeed * deltaTime;
-            Vec3.add(this._curPos, this._curPos, this._deltaPos);   
-            this.node.setPosition(this._curPos);
-        }
+       
+    }
+    onOnceJumpEnd() {
+        this._isMoving = false
     }
 }
 
